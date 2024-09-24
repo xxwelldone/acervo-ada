ca541e15cf833dd071aba72d626e7d7b9969fccd
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-09-24 19:04:19 -0300

    First commit

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,5 @@
 // Interface: Localizavel - Implementada por classes que possuem localização física.
 // Função Genérica: registrarItem - Permite adicionar qualquer tipo de ItemAcervo ao acervo, garantindo a tipagem correta.
 // Lógica de Negócio: Implementada para garantir o funcionamento correto do sistema,  validação de disponibilidade de itens.
+
+//teste
