f2bcfe3ab07d81b15774cd4f233a3e274f996dab
Lidor Brosh <lidorbrosh@gmail.com>
2024-09-27 23:41:38 -0300

    Adição da classe CD com método para detalhes

--- src/Models/cd.ts
+++ src/Models/cd.ts
@@ -1,1 +1,14 @@
-export class CD {}
+import { Midia } from './midia';
+
+export class CD extends Midia {
+    private artista: string;
+
+    constructor(id: number, titulo: string, anoPublicacao: number, duracao: number, artista: string) {
+        super(id, titulo, anoPublicacao, duracao);
+        this.artista = artista;
+    }
+
+    obterDetalhes(): string {
+        return `CD: ${this.titulo} por ${this.artista}, Duração: ${this.duracao} minutos, Ano: ${this.anoPublicacao}, Localização: ${this.obterLocalizacao()}`;
+    }
+}
