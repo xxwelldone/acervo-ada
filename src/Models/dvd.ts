b48cdc9b0dcb9c8dc923034eabc1469abba93fac
Lidor Brosh <lidorbrosh@gmail.com>
2024-09-27 23:41:59 -0300

    Adição da classe DVD com método para detalhes

--- src/Models/dvd.ts
+++ src/Models/dvd.ts
@@ -1,1 +1,14 @@
-export class DVD {}
+import { Midia } from './midia';
+
+export class DVD extends Midia {
+    private diretor: string;
+
+    constructor(id: number, titulo: string, anoPublicacao: number, duracao: number, diretor: string) {
+        super(id, titulo, anoPublicacao, duracao);
+        this.diretor = diretor;
+    }
+
+    obterDetalhes(): string {
+        return `DVD: ${this.titulo}, Diretor: ${this.diretor}, Duração: ${this.duracao} minutos, Ano: ${this.anoPublicacao}, Localização: ${this.obterLocalizacao()}`;
+    }
+}
