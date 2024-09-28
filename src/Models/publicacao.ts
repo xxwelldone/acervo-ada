64b568a9830f6a7cdf0aa802876e0213434c4fbf
Lidor Brosh <lidorbrosh@gmail.com>
2024-09-27 23:43:22 -0300

    Adição da classe Publicacao com informações da editora

--- src/Models/publicacao.ts
+++ src/Models/publicacao.ts
@@ -1,1 +1,25 @@
-export class Publicacao {}
+import { ItemAcervo } from './ItemAcervo';
+import { Localizavel } from '../interfaces/localizavel';
+
+export class Publicacao extends ItemAcervo implements Localizavel {
+    protected editora: string;
+    protected localizacao: string;
+
+    constructor(id: number, titulo: string, anoPublicacao: number, editora: string) {
+        super(id, titulo, anoPublicacao);
+        this.editora = editora;
+        this.localizacao = '';
+    }
+
+    obterLocalizacao(): string {
+        return this.localizacao;
+    }
+
+    definirLocalizacao(localizacao: string): void {
+        this.localizacao = localizacao;
+    }
+
+    obterDetalhes(): string {
+        return `Publicação: ${this.titulo}, Editora: ${this.editora}, Ano: ${this.anoPublicacao}, Localização: ${this.localizacao}`;
+    }
+}
