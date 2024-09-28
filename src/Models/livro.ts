c66542f3b03d7fa14b17873cd016abdf2de1c766
Lidor Brosh <lidorbrosh@gmail.com>
2024-09-27 23:42:21 -0300

    Criação da classe Livro com detalhes de ISBN e autor

--- src/Models/livro.ts
+++ src/Models/livro.ts
@@ -1,1 +1,24 @@
-export class Livro {}
+import { Publicacao } from './publicacao';
+
+export class Livro extends Publicacao {
+    private isbn: string;
+    private autor: string;
+
+    constructor(id: number, titulo: string, anoPublicacao: number, editora: string, isbn: string, autor: string) {
+        super(id, titulo, anoPublicacao, editora);
+        this.isbn = isbn;
+        this.autor = autor;
+    }
+
+    obterDetalhes(): string {
+        return `Livro: ${this.titulo} por ${this.autor}, ISBN: ${this.isbn}, Ano: ${this.anoPublicacao}, Editora: ${this.editora}, Localização: ${this.obterLocalizacao()}`;
+    }
+
+    getAutor(): string {
+        return this.autor;
+    }
+
+    getIsbn(): string {
+        return this.isbn;
+    }
+}
