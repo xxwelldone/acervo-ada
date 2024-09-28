dc3efdb7ea025b57bbfe5098c15173338c681c44
Lidor Brosh <lidorbrosh@gmail.com>
2024-09-27 23:43:52 -0300

    Criação da classe Revista

--- src/Models/revista.ts
+++ src/Models/revista.ts
@@ -1,1 +1,18 @@
-export class Revista {}
+import { Publicacao } from './publicacao';
+
+export class Revista extends Publicacao {
+    private edicao: string;
+
+    constructor(id: number, titulo: string, anoPublicacao: number, editora: string, edicao: string) {
+        super(id, titulo, anoPublicacao, editora);
+        this.edicao = edicao;
+    }
+
+    obterDetalhes(): string {
+        return `Revista: ${this.titulo}, Edição: ${this.edicao}, Ano: ${this.anoPublicacao}, Editora: ${this.editora}, Localização: ${this.obterLocalizacao()}`;
+    }
+
+    getEdicao(): string {
+        return this.edicao;
+    }
+}
