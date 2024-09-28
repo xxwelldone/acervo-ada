26100e27b4f33a510a058b840c5882d50afec153
Lidor Brosh <lidorbrosh@gmail.com>
2024-09-27 23:42:40 -0300

    Criação da classe Midia e implementação da interface Localizavel

--- src/Models/midia.ts
+++ src/Models/midia.ts
@@ -1,1 +1,25 @@
-export class Midia {}
+import { ItemAcervo } from './ItemAcervo';
+import { Localizavel } from '../interfaces/localizavel';
+
+export class Midia extends ItemAcervo implements Localizavel {
+    protected duracao: number;
+    protected localizacao: string;
+
+    constructor(id: number, titulo: string, anoPublicacao: number, duracao: number) {
+        super(id, titulo, anoPublicacao);
+        this.duracao = duracao;
+        this.localizacao = '';
+    }
+
+    obterLocalizacao(): string {
+        return this.localizacao;
+    }
+
+    definirLocalizacao(localizacao: string): void {
+        this.localizacao = localizacao;
+    }
+
+    obterDetalhes(): string {
+        return `Mídia: ${this.titulo}, Duração: ${this.duracao} minutos, Ano: ${this.anoPublicacao}, Localização: ${this.localizacao}`;
+    }
+}
